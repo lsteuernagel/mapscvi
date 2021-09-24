06c262e6a52673f5e4f170804e14085877ff70a1
lsteuernagel <lsteuernagel@blade10.sf.mpg.de>
2021-09-24 10:43:19 +0200

    adding quality and utility functions

---
+++ R/quality_functions.R
@@ -0,0 +1,200 @@
+##########
+### check_distance_neighbors
+##########
+
+#' Estimate quality of mapped data
+#'
+#' Calculates a shared KNN to see how well query cells mix with reference cells and how the distances are.
+#'
+#' @inheritParams project_query
+#' @param reference_seurat reference seurat
+#' @param reduction_name name of scvi reduction in reference AND query. Also expect a umap named as 'umap_'reference_reduction
+#' @param query_nn character vector with name
+#' @param reference_nn character vector with name
+#' @param k_param k neighbors
+#' @param annoy.metric string passed to Seurat's FindNeighbors
+#'
+#' @return query_seurat_object with quality results in metadata
+#' median_dist_to_neighbors: what is the median distance of a query cell to all neighbor cells
+#' median_dist_refNeighbors: for all neighbor cells from the reference: what are their median distances
+#' normalized_nn_distance: median_dist_to_neighbors / median_dist_refNeighbors (how comparable are the query cells distances to their neighboring cell distances)
+#'
+#' @export
+#'
+#' @import SeuratObject Seurat dplyr
+#'
+#' @examples
+
+check_distance_neighbors = function(query_seurat_object,reference_seurat,reduction_name="scvi",query_nn = "query_ref_nn",reference_nn = "reference_nn",k_param=30,annoy.metric="cosine",global_seed=12345){
+
+  # check that reduction_name exists
+  #TODO
+
+  # add query boolean:
+  query_seurat_object@meta.data$query =TRUE
+
+  # query
+  if(query_nn %in%  names(query_seurat_object@neighbors)){
+    message("Found ",query_nn)
+  }else{
+    query_seurat_object = Seurat::FindNeighbors(query_seurat_object,reduction =  reduction_name,k.param = k_param,graph.name = query_nn,annoy.metric = annoy.metric,
+                                        return.neighbor = TRUE,dims = 1:ncol(query_seurat_object@reductions[[reduction_name]]@cell.embeddings))
+  }
+  query_to_ref_idx = query_seurat_object@neighbors[[query_nn]]@nn.idx
+  query_to_ref_dists = query_seurat_object@neighbors[[query_nn]]@nn.dist
+
+  # ref
+  if(reference_nn %in%  names(reference_seurat@neighbors)){
+    message("Found ",reference_nn)
+  }else{
+    reference_seurat = Seurat::FindNeighbors(reference_seurat,reduction =  reduction_name,k.param = k_param,graph.name = reference_nn,annoy.metric = annoy.metric,
+                                     return.neighbor = TRUE,dims = 1:ncol(reference_seurat@reductions[[reduction_name]]@cell.embeddings))
+  }
+  ref_idx = reference_seurat@neighbors[[reference_nn]]@nn.idx
+  ref_dists = reference_seurat@neighbors[[reference_nn]]@nn.dist
+
+  # get average distance to all neighbors of query cells
+  query_dists = apply(query_to_ref_dists[which(query_seurat_object@meta.data$query),2:ncol(query_to_ref_dists)],1,median)
+  query_dists = data.frame(Cell_ID = query_seurat_object@meta.data$Cell_ID[query_seurat_object@meta.data$query], median_dist_to_neighbors = query_dists)
+
+  # for each query cell: get average dist of its reference neighbors
+  query_neighbor_dists = apply(query_to_ref_idx[which(query_seurat_object@meta.data$query),2:ncol(query_to_ref_idx)],1,function(x,nn_dist){
+    median(apply(nn_dist[x,2:ncol(nn_dist)],1,median))
+  },nn_dist=ref_dists)
+  # summarise
+  query_neighbor_dists = data.frame(Cell_ID = query_seurat_object@meta.data$Cell_ID[query_seurat_object@meta.data$query], median_dist_refNeighbors = query_neighbor_dists)
+
+  # compute normalized score
+  query_seurat_object@meta.data$normalized_nn_distance = query_dists$median_dist_to_neighbors / query_neighbor_dists$median_dist_refNeighbors
+  message("Adding result to metadata as: ","normalized_nn_distance")
+
+  # return
+  return(query_seurat_object)
+
+}
+
+##########
+### check_freq_neighbors
+##########
+
+#' Estimate quality of mapped data
+#'
+#' Calculates a shared KNN to see how well query cells mix with reference cells and how the distances are.
+#'
+#' @inheritParams project_query
+#' @inheritParams check_distance_neighbors
+#' @param reference_seurat reference seurat
+#'
+#' @return query_seurat_object with quality results in metadata
+#' query_neighbor_pct: how many neighbors are from query dataset. Can either indicate that query contains many more cells than reference or that celltype is absent in reference.
+#'
+#' @export
+#'
+#' @import SeuratObject Seurat dplyr
+#'
+#' @examples
+
+check_freq_neighbors = function(query_seurat_object,reference_seurat,reduction_name="scvi",k_param=30,annoy.metric="cosine",global_seed=12345){
+
+  # check that reduction_name exists
+  #TODO
+
+  # add query boolean:
+  query_seurat_object@meta.data$query =TRUE
+  # merge objects
+  merged_object = merge(reference_seurat,query_seurat_object,merge.dr = reduction_name)
+  # set query variable to false
+  merged_object@meta.data$query[is.na(merged_object@meta.data$query)] =FALSE
+  # need to run FindNeighbors
+  merged_object = Seurat::FindNeighbors(merged_object,reduction =  reduction_name,k.param = k_param,graph.name = "new_nn",annoy.metric = annoy.metric,
+                                return.neighbor = TRUE,dims = 1:ncol(merged_object@reductions[[reduction_name]]@cell.embeddings))
+  nn_idx = merged_object@neighbors$new_nn@nn.idx
+  nn_dist = merged_object@neighbors$new_nn@nn.dist
+
+  # use nn_idx to get number of query neighbors for each query cells
+  query_freq = apply(nn_idx[which(merged_object@meta.data$query),2:ncol(nn_idx)],1,function(row,query){
+    freq_batch = sum(query[row]) / length(row)
+  }, query = merged_object@meta.data$query)
+  query_freq = data.frame(Cell_ID = merged_object@meta.data$Cell_ID[merged_object@meta.data$query], query_neighbor_pct = query_freq)
+
+  ## add result
+  query_seurat_object@meta.data$query_neighbor_freq = query_freq$query_neighbor_pct
+  message("Adding result to metadata as: ","query_neighbor_freq")
+  # return
+  return(query_seurat_object)
+
+}
+
+##########
+### check_reference_markers_per_cell
+##########
+
+#' Estimate quality of mapped data based on marker genes from reference
+#'
+#' Check marker gene expression in query cells based on predicted clusters and markers from reference.
+#'
+#' @inheritParams project_query
+#' @inheritParams check_distance_neighbors
+#' @param marker_genes a named list of character vectors with (marker) gene names and names corresponding to values in query_label_col
+#' @param query_label_col string, column-name in metadata of query_seurat_object. values of have to correspond to names of marker_genes
+#' @param min_expr min expression to be considered expressed (defaults to 0)
+#'
+#' @return query_seurat_object with quality results in metadata
+#' marker_pct: how many markers of projected reference cluster are expressed in each cell
+#'
+#' @export
+#'
+#' @import SeuratObject Seurat dplyr Matrix
+#'
+#' @examples
+
+check_reference_markers_per_cell = function(query_seurat_object,marker_genes,assay="RNA",query_label_col="predicted",min_expr=0,global_seed=12345){
+
+  # check input arguments
+  if(!query_label_col %in% colnames(query_seurat_object@meta.data)){
+    stop("Error: Cannot find '",query_label_col,"' in query_seurat_object@meta.data. Stopping.")
+  }else{
+    all_labels = unique(query_seurat_object@meta.data[,query_label_col])
+  }
+
+  if(length(setdiff(all_labels,names(marker_genes)))>0){
+    stop("Warning: Some labels are not part of marker_genes list (cannot find in list names!)")
+  }
+  # run pct of expressed markers per cell
+  res_marker_freq_list = sapply(all_labels,function(label,all_labels,count_matrix,marker_genes){
+    label_idx = which(all_labels == label)
+    label_genes = marker_genes[[label]]
+    if(length(label_genes)>1){
+      subset_matrix = count_matrix[rownames(count_matrix) %in% label_genes,label_idx]
+      subset_matrix[subset_matrix>min_expr] = 1
+      if(length(label_idx)>1){
+        freq_df = data.frame(Cell_ID = colnames(subset_matrix), marker_pct = Matrix::colSums(subset_matrix) / length(label_genes))
+      }else if(length(label_idx)==1){
+        freq_df = data.frame(Cell_ID = colnames(count_matrix)[label_idx], marker_pct = sum(subset_matrix) / length(label_genes))
+      }else{
+        freq_df =NULL
+      }
+    }else{
+      freq_df =NULL
+    }
+    freq_df
+  },all_labels=query_seurat_object@meta.data[,query_label_col],count_matrix=query_seurat_object@assays[[assay]]@data,marker_genes=marker_genes,simplify = FALSE)
+  res_marker_freq = do.call(rbind,res_marker_freq_list)
+
+  ## add to metadata
+  if("marker_pct" %in% colnames(query_seurat_object@meta.data)){
+    query_seurat_object@meta.data = query_seurat_object@meta.data %>% dplyr::select(-marker_pct)
+    rownames(query_seurat_object@meta.data) = query_seurat_object@meta.data$Cell_ID
+  }
+  query_seurat_object@meta.data = dplyr::left_join(query_seurat_object@meta.data,res_marker_freq,by="Cell_ID")
+  rownames(query_seurat_object@meta.data) = query_seurat_object@meta.data$Cell_ID
+  message("Adding result to metadata as: ","marker_pct")
+
+  return(query_seurat_object)
+
+}
+
+
+
+
+
---
+++ R/utility_functions.R
@@ -0,0 +1,127 @@
+##########
+### reconstruct_levels
+##########
+
+#' Reconstruct high cluster levels based on provided edgelist
+#'
+#' After prediction or label propagation using a low clustering level this function can be used to reconstruct higher clustering levels.
+#' Is a wrapper around 'recursive_reconstruction'
+#' Requires a clustertree defined in a simple edgelist. User has to ensure that provided input is a valid edgelist!
+#'
+#' @param edgelist dataframe or matrix with two columns (edgelist: to and from). optional third column giving the 'level' of 'from' which will be used to determine reconstructed levels and name columns
+#' @param input_annotation query project name. defaults to 'query'
+#' @param level_prefix regex used to extract 'level' if third column is missing
+#' @param result_prefix prefix to add to result column names
+#'
+#' @return dataframe with columns for input_annotation and all clustering levels (above) in edgelist
+#'
+#' @export
+#'
+#' @import stringr
+#'
+#' @examples
+
+reconstruct_levels = function(edgelist,input_annotation,level_prefix="K[0-9]+",result_prefix = ""){
+
+  # TODO: I am not checking if edgelist is truly a valid edgelist!
+
+  # check columns of edgelist and prepare level
+  if(ncol(edgelist)==1){
+    stop("Error: Please provide a dataframe or matrix with the first two columns correpsonding to 'to' and 'from'")
+  }else if(ncol(edgelist)==2){
+    colnames(edgelist)[1:2] = c("from","to")
+    # infer level
+    message("Inferring level with prefix: '",level_prefix,"'")
+    edgelist$level = stringr::str_extract(string = edgelist$from,pattern = level_prefix)
+    if(all(is.na(edgelist$level))){stop("Error: Cannot infer level. Stopping execution.")}
+  }else if(ncol(edgelist)>2){
+    # subset to first three and rename third
+    edgelist = edgelist[,1:3]
+    colnames(edgelist)[1:3] = c("from","to","level")
+  }
+
+  # check that input_annotation are part of to
+  if(length(setdiff(unique(input_annotation),unique(edgelist$to)))>0){
+    stop("Error: Some input annotations are not part of provided edgelist: ",paste0(setdiff(unique(input_annotation),unique(edgelist$to)),collapse = " | "))
+  }
+  # reconstruct levels
+  res= recursive_reconstruction(edgelist,input_annotation)
+  colnames(res) = paste0(result_prefix,colnames(res))
+  #return
+  return(res)
+}
+
+##########
+### recursive_reconstruction
+##########
+
+#' Reconstruct high cluster levels based on provided edgelist
+#'
+#' Recursively go through edgelist and reconstruct higher-level annotation of input_annotation
+#'
+#' @param edgelist dataframe or matrix with two columns (edgelist: to and from). optional third column giving the 'level' of 'from' which will be used to determine reconstructed levels and name columns
+#' @param input_annotation query project name. defaults to 'query'
+#'
+#' @return matrix with columns for input_annotation and all clustering levels (above) in edgelist
+#'
+#' @export
+#'
+#' @examples
+
+recursive_reconstruction = function(edgelist,input_annotation){
+  current_level = edgelist$level[edgelist$to==input_annotation[1]]
+  current_annotation = as.character(sapply(input_annotation,function(x,edgelist){edgelist$from[edgelist$to == x]},edgelist=edgelist))
+  if(current_level %in% edgelist$level & !is.na(current_level)){
+    next_annotation = recursive_reconstruction(edgelist,current_annotation)
+    df = cbind(current_annotation,next_annotation)
+    colnames(df)[1]=current_level
+  }else{
+    df = NULL
+  }
+  return(df)
+}
+
+
+##########
+### add_paired_annotation
+##########
+
+#' Return annotation B based on annotation A
+#'
+#' ..
+#'
+#' @param input_annotation vector of input annotation A
+#' @param reference_mapping dataframe with mapping of annotations A and B. if NULL will be inferred from reference_annotations
+#' @param reference_annotations dataframe with all annotations A and B. Will be used to build reference_mapping. Defaults to NULL. Ignored if reference_mapping is provided or NULL.
+#'
+#' @return vector of same length as input_annotation with annotation B (based on annotation A)
+#'
+#' @export
+#'
+#' @examples
+
+add_paired_annotation = function(input_annotation,reference_mapping=NULL,reference_annotations=NULL){
+
+  # if no reference mapping: infer
+  if(is.null(reference_mapping)){
+    if(!is.null(reference_annotations)){
+      reference_mapping = reference_annotations[!duplicated(reference_annotations[,1:2]),]
+      rownames(reference_mapping) = reference_mapping[,1]
+    }else{
+      stop("Error: Please provide either reference_mapping or reference_annotations.")
+    }
+  }
+  # check mapping
+  if(max(table(reference_mapping[,1])) > 1 | max(table(reference_mapping[,2])) > 1){
+    stop("Error: Please provide a reference_mapping with 1:1 mappings.")
+  }
+  # check that input_annotation is in reference_mapping
+  if(length(setdiff(unique(input_annotation),unique(reference_mapping[,1])))>0){
+    stop("Error: Some input annotations are not part of first column of provided reference_mapping: ",paste0(setdiff(unique(input_annotation),unique(reference_mapping[,1])),collapse = " | "))
+  }
+  # infer annotation B
+  new_annotation = as.character(sapply(input_annotation,function(x,reference_mapping){reference_mapping[reference_mapping[,1] == x,2]},reference_mapping=reference_mapping))
+  #return vector
+  return(new_annotation)
+}
+
