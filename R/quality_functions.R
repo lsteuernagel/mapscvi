9fa1df69676997b26eeac2c926903f9640620d6f
lsteuernagel <lsteuernagel@blade10.mpin-koeln.mpg.de>
2021-10-14 09:46:44 +0200

    QC functions

--- R/quality_functions.R
+++ R/quality_functions.R
@@ -48,12 +48,19 @@ check_distance_neighbors = function(query_seurat_object,reference_seurat,reducti
     message("Found ",reference_nn)
   }else{
     reference_seurat = Seurat::FindNeighbors(reference_seurat,reduction =  reduction_name,k.param = k_param,graph.name = reference_nn,annoy.metric = annoy.metric,
-                                     return.neighbor = TRUE,dims = 1:ncol(reference_seurat@reductions[[reduction_name]]@cell.embeddings))
+                                             return.neighbor = TRUE,dims = 1:ncol(reference_seurat@reductions[[reduction_name]]@cell.embeddings))
   }
   ref_idx = reference_seurat@neighbors[[reference_nn]]@nn.idx
   ref_dists = reference_seurat@neighbors[[reference_nn]]@nn.dist
-
-  message("Calculating average distances ...")
+#
+#   message("Calculating average distances ...")
+#   mean_between_distance_reference = apply(neighbors_reference_object@nn.idx,1,function(idx_of_neighbor,latent_space){
+#     distances_of_ref_neighbors = as.matrix(dist( latent_space[idx_of_neighbor,],method = "euclidean"))
+#     mean(distances_of_ref_neighbors[upper.tri(distances_of_ref_neighbors)])
+#   },latent_space=latent_space)
+#
+#
+#
   # get average distance to all neighbors of query cells
   query_dists = apply(query_to_ref_dists[which(query_seurat_object@meta.data$query),2:ncol(query_to_ref_dists)],1,stats::median)
   query_dists = data.frame(Cell_ID = query_seurat_object@meta.data$Cell_ID[query_seurat_object@meta.data$query], median_dist_to_neighbors = query_dists)
@@ -110,7 +117,7 @@ check_freq_neighbors = function(query_seurat_object,reference_seurat,reduction_n
   # need to run FindNeighbors
   message("Calculating shared neighbors ...")
   merged_object = Seurat::FindNeighbors(merged_object,reduction =  reduction_name,k.param = k_param,graph.name = "new_nn",annoy.metric = annoy.metric,
-                                return.neighbor = TRUE,dims = 1:ncol(merged_object@reductions[[reduction_name]]@cell.embeddings))
+                                        return.neighbor = TRUE,dims = 1:ncol(merged_object@reductions[[reduction_name]]@cell.embeddings))
   nn_idx = merged_object@neighbors$new_nn@nn.idx
   nn_dist = merged_object@neighbors$new_nn@nn.dist
 
@@ -199,6 +206,139 @@ check_reference_markers_per_cell = function(query_seurat_object,marker_genes,ass
 }
 
 
+##########
+### adjusted_cell_probabilities
+##########
+
+#' Estimate quality of mapped data based on marker genes from reference
+#'
+#' Cell probabilities similar to scARches algorithm
+#' This function runs per cell
+#'
+#' @param dist_Nc vector of length k with euclidean distances to neigbors
+#' @param label_of_neighbor vector of length k with labels of neighbors
+#' @param result_type return type: "all","label","entropy"
+#'
+#' @return label probability depending on result_type
+#'
+#' @export
+
+adjusted_cell_probabilities = function(dist_Nc,labels_of_neighbor,result_type="all"){
+
+  # step 1: Distances and input
+  # ... input of function
+  local_labels = unique(labels_of_neighbor)
+  k = length(labels_of_neighbor)
+  # step 2: compute the standard deviation of the nearest distances
+  sd_nc = sqrt(sum(dist_Nc^2) / k )
+  # step 3: apply Gaussian kernel to distances
+  d_app = exp(-1*(dist_Nc/(2/sd_nc)^2) )
+  # step 4: we computed the probability of assigning each label y to the query cell c by normalizing across all adjusted distances
+  label_probabilities = tapply(d_app,INDEX = labels_of_neighbor,FUN = sum) / sum(d_app)
+  # return result
+  if(result_type == "all"){
+    return(label_probabilities)
+    # or summarise further before returning:
+  }else{
+    if(result_type == "label"){
+      # step 5: uncertainty score: 1 - prob_label --> I rather return directly the highest label with its name
+      return(label_probabilities[label_probabilities == max(label_probabilities)])
+    }else if(result_type == "entropy"){
+      # step 6: try out entropy:
+      ## entropy helper:
+      entropy_fun = function(x,logfun ="log2"){
+        log_vec = do.call(logfun,list(x))
+        log_vec[is.infinite(log_vec)] = 0
+        log_vec[is.nan(log_vec)] = 0
+        return(-sum(x * log_vec))
+      }
+      entropy_uncertainty = entropy_fun(label_probabilities) / max(1,log2(length(local_labels)))
+      return(entropy_uncertainty)
+    }else{
+      return(NA)
+    }
+  }
+
+}
+
+##########
+### propagate_labels_prob
+##########
+
+#' Estimate quality of mapped data based on marker genes from reference
+#'
+#' Cell probabilities similar to scARches algorithm
+#' This function runs per cell
+#'
+#' @param query_seurat_object
+#' @param reference_seurat_object v
+#' @param label_col
+#' @param reduction_name_query
+#' @param reduction_name_reference
+#' @param k.param
+#' @param add_entropy
+#' @param add_to_seurat
+#'
+#' @return object
+#'
+#' @export
+
+propagate_labels_prob = function(query_seurat_object,reference_seurat_object,label_col,reduction_name_query="scvi",reduction_name_reference="scvi",k.param=30, add_entropy =FALSE, add_to_seurat =TRUE){
+
+  # need euclidean distances neighbors
+  neighbors_object = Seurat::FindNeighbors(reference_seurat_object@reductions[[reduction_name_reference]]@cell.embeddings,
+                                           query = query_seurat_object@reductions[[reduction_name_query]]@cell.embeddings,
+                                           k.param = k.param, return.neighbor =TRUE,
+                                           annoy.metric="euclidean")
+
+  # define label vector
+  all_labels = reference_seurat_object@meta.data[,label_col]
+
+  # apply max prob per cell function
+  message("Estimate probabilities")
+  n=nrow(neighbors_object@nn.dist)
+  max_probabilities = sapply(1:n,function(x,distances,neighbor_idxs,labels){
+    dist_Nc = distances[x,]
+    label_of_neighbor = labels[neighbor_idxs[x,]]
+    prob = adjusted_cell_probabilities(dist_Nc = dist_Nc,labels_of_neighbor = label_of_neighbor,result_type = "label")
+    prob
+  },distances = neighbors_object@nn.dist,neighbor_idxs = neighbors_object@nn.idx,labels = all_labels)
+
+  # mapping results
+  mapping_results = data.frame(Cell_ID = neighbors_object@cell.names[1:n], predicted = names(max_probabilities), prediction_probability = as.numeric(max_probabilities))
+
+  # not super efficient because I apply the function twice ....
+  if(add_entropy){
+    message("Estimate entropy")
+    # apply max prob per cell function
+    mapping_results$prediction_entropy = sapply(1:n,function(x,distances,neighbor_idxs,labels){
+      dist_Nc = distances[x,]
+      label_of_neighbor = labels[neighbor_idxs[x,]]
+      prob = adjusted_cell_probabilities(dist_Nc = dist_Nc,labels_of_neighbor = label_of_neighbor,result_type = "entropy")
+      prob
+    },distances = neighbors_object@nn.dist,neighbor_idxs = neighbors_object@nn.idx,labels = all_labels)
+  }
+  # return
+  if(add_to_seurat){
+    # remove if existing for clean join
+    keep_names = colnames(query_seurat_object@meta.data)[!colnames(query_seurat_object@meta.data) %in% c("predicted","prediction_probability","prediction_entropy")]
+    query_seurat_object@meta.data = query_seurat_object@meta.data[,keep_names]
+    # join
+    query_seurat_object@meta.data = dplyr::left_join(query_seurat_object@meta.data, mapping_results, by = c("Cell_ID"="Cell_ID"))
+    # set rownames of dataframe after dplyr
+    rownames(query_seurat_object@meta.data) = query_seurat_object@meta.data$Cell_ID
+    return(query_seurat_object)
+  }else{
+    return(mapping_results)
+  }
+}
 
 
+query_snseq_neurons = propagate_labels_prob(query_seurat_object = query_snseq_neurons,
+                                            reference_seurat_object = neuron_map_seurat,
+                                            label_col = "K329_pruned",k.param = 30,add_to_seurat = TRUE,add_entropy = TRUE)
+query_snseq_neurons$prediction_entropy = 1 - query_snseq_neurons$prediction_entropy
+DimPlot(query_snseq_neurons,group.by = "predicted",reduction = "umap_scvi")+NoLegend()+NoAxes()
+FeaturePlot(query_snseq_neurons,features = "prediction_probability")
+FeaturePlot(query_snseq_neurons,features = "prediction_entropy")
 
