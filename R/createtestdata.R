d194a1fef7acddeac220ddef7fa20dd5f7390d5f
lsteuernagel <lsteuernagel@blade10.mpin-koeln.mpg.de>
2021-11-02 14:06:22 +0100

    downsample reference objects script

--- R/createtestdata.R
+++ R/createtestdata.R
@@ -20,14 +20,16 @@
 # reference_hypoMap@assays$RNA@scale.data <- dummy[,-1] # error is okay
 # reference_hypoMap@assays$RNA@meta.features <- as.data.frame(dummy[,-1])
 # print(object.size(reference_hypoMap) / 1000000)
-# reference_map_reduc = neuron_map_seurat@reductions$scvi
-# object.size(reference_map_reduc) / 1000000
-# object.size(reference_map_metadata) / 1000000
-# reference_map_umap = neuron_map_seurat@reductions$umap_scvi
-# reference_map_metadata = neuron_map_seurat@meta.data
-# object.size(reference_map_metadata) / 1000000
-# object.size(reference_map_umap) / 1000000
-# reference_hypoMap_neurons=reference_hypoMap
+# reference_hypoMap_neurons = reference_hypoMap
+#
+# ## downsample:
+# ## source("/beegfs/scratch/bruening_scratch/lsteuernagel/projects/scHarmonize/utils.R") or See function below!!!!
+# cluster_downsample = downsample_balanced_iterative(reference_hypoMap_neurons@meta.data,target_sample_size = 20000,predictor_var = "K169_pruned",stepsize = 500,global_seed = 12345)
+# table(cluster_downsample$K169_pruned)
+#
+# reference_hypoMap_neurons = subset(reference_hypoMap_neurons,cells = cluster_downsample$Cell_ID)
+# print(object.size(reference_hypoMap_neurons) / 1000000)
+#
 # save(reference_hypoMap_neurons,file = "/beegfs/scratch/bruening_scratch/lsteuernagel/projects/mapscvi/data/reference_hypoMap_neurons.RData")
 #
 # # set model
@@ -58,13 +60,24 @@
 # reference_hypoMap_full@assays$RNA@scale.data <- dummy[,-1] # error is okay
 # reference_hypoMap_full@assays$RNA@meta.features <- as.data.frame(dummy[,-1])
 # print(object.size(reference_hypoMap_full) / 1000000)
+#
+# ## downsample:
+# ## source("/beegfs/scratch/bruening_scratch/lsteuernagel/projects/scHarmonize/utils.R") or See function below!!!!
+# cluster_downsample = downsample_balanced_iterative(reference_hypoMap_full@meta.data,target_sample_size = 20000,predictor_var = "Curated_CellType",
+#                                                    stepsize = 500,global_seed = 12345)
+# table(cluster_downsample$Curated_CellType)
+#
+# reference_hypoMap_full = subset(reference_hypoMap_full,cells = cluster_downsample$Cell_ID)
+# print(object.size(reference_hypoMap_full) / 1000000)
+#
+# # save
 # save(reference_hypoMap_full,file = "/beegfs/scratch/bruening_scratch/lsteuernagel/projects/mapscvi/data/reference_hypoMap_full.RData")
 #
 # # set model
 # model_path = "/beegfs/scratch/bruening_scratch/lsteuernagel/data/hypoMap/hypoMap_models/scVI_hypothalamus_full_map_model/"
 # system(paste0("cp -r ",model_path," /beegfs/scratch/bruening_scratch/lsteuernagel/projects/mapscvi/inst/extdata/models/"))
 #
-
+#
 # #####
 # ## load example data
 # #####
@@ -88,3 +101,40 @@
 # #save testdata
 # save(query_romanov,file = "/beegfs/scratch/bruening_scratch/lsteuernagel/projects/mapscvi/data/query_romanov_object.RData")
 # save(sce_lamanno_da,file = "/beegfs/scratch/bruening_scratch/lsteuernagel/projects/mapscvi/data/sce_lamanno_da.RData")
+#
+# #####
+# ## function for downsampling
+# #####
+#
+# downsample_balanced_iterative <- function(metadata,target_sample_size,predictor_var,stepsize =100,global_seed=1234){
+#   message("Running downsample_balanced_iterative")
+#   require(dplyr)
+#   metadata = as.data.frame(metadata)
+#   # group and count full data
+#   probs_for_sample = metadata %>% dplyr::ungroup() %>% dplyr::group_by(!!dplyr::sym(predictor_var)) %>% dplyr::add_count(name = "per_group") %>% dplyr::select(Cell_ID,per_group,!!dplyr::sym(predictor_var))
+#   probs_for_sample = as.data.frame(probs_for_sample)
+#   # first: remove all NAs
+#   probs_for_sample = probs_for_sample[!is.na(probs_for_sample[,predictor_var]),]
+#   # init
+#   sizes = seq(from=nrow(probs_for_sample), to=target_sample_size,by = -stepsize)
+#   #print(sizes)
+#   n_classes = length(unique(probs_for_sample[,predictor_var]))
+#   tmp_samples=probs_for_sample
+#   # loop over sizes and remove iteratively
+#   cc=0
+#   for(size in sizes[1:(length(sizes)-1)]){
+#     cc=cc+1
+#     # find classes that should be left out
+#     which_to_downsample = table(tmp_samples[,predictor_var])>(size/n_classes)
+#     # addiotionally add some other classes that are close
+#     which_to_downsample = c(which_to_downsample,table(tmp_samples[,predictor_var])>min(table(tmp_samples[,predictor_var])[names(which_to_downsample[which_to_downsample])]))
+#     # sample from these two classes
+#     set.seed(global_seed)
+#     leave_out = sample(tmp_samples$Cell_ID[tmp_samples[,predictor_var] %in% names(which_to_downsample[which_to_downsample])],stepsize)
+#     # subset
+#     tmp_samples = tmp_samples[!tmp_samples$Cell_ID %in% leave_out,]
+#   }
+#
+#   # return samples IDs
+#   return(tmp_samples)
+# }
